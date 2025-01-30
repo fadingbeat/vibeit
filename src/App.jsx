fc68be9ed2c405dd113f795045b31a50bd978e5e
fadingbeat <fadingbeat@gmail.com>
2025-01-30 12:28:13 +0100

    Removed component imports in main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import './App.css';
 import React from 'react';
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import Services from './components/Services';
-import Footer from './components/Footer';
-import OurWork from './components/OurWork';
-import Contact from './components/contact';
 import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div>
       <Hero />
-      {/* <Header />
-      <HeroSection />
-      <Services />
-      <OurWork />
-      <Contact />
-      <Footer /> */}
     </div>
   );
 }
